01e10e392ebabc2fad8d785ca14eac0afbf182a2
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-31 15:22:49 -0500

    creates movies router

---
+++ server/routes/movies.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+//GET ALL MOVIES
+
+//GET ONE MOVIE
+
+//PUT FOR SINGLE MOVIE
+
+module.exports = router;
