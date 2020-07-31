9b0ed6b271b4a4bc0ce28ebca63810e88505325d
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-31 15:32:00 -0500

    creates get all movies in router

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 //GET ALL MOVIES
-
+router.get('/', (req, res) => {
+  const queryText = `SELECT * FROM "movies" ORDER BY "title" ASC;`;
+  pool.query(queryText);
+});
 //GET ONE MOVIE
 
 //PUT FOR SINGLE MOVIE
