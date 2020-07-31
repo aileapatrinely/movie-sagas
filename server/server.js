50fe0446bac2458fd0d8f481b63b395cd8966a1e
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-31 15:23:38 -0500

    imports movies router into server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const router = require('./routes/movies.router');
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -8,9 +9,9 @@ app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/api/movies', router);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
-    console.log('Listening on port: ', port);
+  console.log('Listening on port: ', port);
 });
