dc0c15ae701735826e7f4ada82ebf8b3a9715d14
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-31 15:39:32 -0500

    creates get router for single movie

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -5,9 +5,28 @@ const pool = require('../modules/pool');
 //GET ALL MOVIES
 router.get('/', (req, res) => {
   const queryText = `SELECT * FROM "movies" ORDER BY "title" ASC;`;
-  pool.query(queryText);
+  pool
+    .query(queryText)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log('Error in ALL MOVIES get router', err);
+    });
 });
 //GET ONE MOVIE
+router.get('/', (req, res) => {
+  const queryText = `SELECT * FROM "movies"
+    WHERE "title"= $1;`;
+  pool
+    .query(quertyText, req.body.title)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log('Error in ONE MOVIE get router', err);
+    });
+});
 
 //PUT FOR SINGLE MOVIE
 
